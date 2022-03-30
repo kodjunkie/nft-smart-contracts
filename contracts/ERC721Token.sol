b3c50a0f53134dc9deb66b9cfd82de9cf0e42862
Lawrence Onah <paplow01@gmail.com>
2022-03-30 21:11:08 +0100

    Improve config

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -1,36 +1,36 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.13;
+pragma solidity >=0.8.13 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract ERC721Token is ERC721Enumerable, Pausable {
-    /**
-     * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
-     */
-    constructor() ERC721("ERC721Token", "ERC") {}
+	/**
+	 * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
+	 */
+	constructor() ERC721("ERC721Token", "ERC") {}
 
-    /**
-     * @dev Mints `tokenId` and transfers it to `to`.
-     *
-     * WARNING: Usage of this method is discouraged, use {_safeMint} whenever possible
-     *
-     * Requirements:
-     *
-     * - `tokenId` must not exist.
-     * - `to` cannot be the zero address.
-     *
-     * Emits a {Transfer} event.
-     */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override {
-        super._beforeTokenTransfer(from, to, tokenId);
+	/**
+	 * @dev Mints `tokenId` and transfers it to `to`.
+	 *
+	 * WARNING: Usage of this method is discouraged, use {_safeMint} whenever possible
+	 *
+	 * Requirements:
+	 *
+	 * - `tokenId` must not exist.
+	 * - `to` cannot be the zero address.
+	 *
+	 * Emits a {Transfer} event.
+	 */
+	function _beforeTokenTransfer(
+		address from,
+		address to,
+		uint256 tokenId
+	) internal virtual override {
+		super._beforeTokenTransfer(from, to, tokenId);
 
-        require(!paused(), "ERC721Pausable: token transfer while paused");
-    }
+		require(!paused(), "ERC721Pausable: token transfer while paused");
+	}
 }
