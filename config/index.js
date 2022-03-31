ec109814c129a24e39a9cb065afcee91114945df
Lawrence Onah <paplow01@gmail.com>
2022-03-31 11:04:40 +0100

    :hammer_and_wrench: Refactoring and improved config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,19 @@
+require("dotenv").config({ path: "../.env" });
+
+const env = process.env;
+
 module.exports = {
+	// Local Network
 	port: 8545,
 	host: "127.0.0.1",
 	network_id: 1337,
+
+	// HD Wallet Provider
+	mnemonic: env.MNEMONIC,
+
+	// Infura project IDs
+	infura: {
+		mainnet: env.INFURA_MAINNET,
+		rinkeby: env.INFURA_RINKEBY,
+	},
 };
