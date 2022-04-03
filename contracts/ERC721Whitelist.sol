f069078595cb4a4ceed84e6ea540ba33b5d724ea
Lawrence Onah <paplow01@gmail.com>
2022-04-03 19:43:44 +0100

    :pencil: Fixed typo

--- contracts/ERC721Whitelist.sol
+++ contracts/ERC721Whitelist.sol
@@ -89,12 +89,12 @@ contract ERC721Whitelist is ERC721, Ownable {
 		}
 	}
 
-	// Activate whitelist sale
+	// Toggle whitelist sales activity
 	function toggleWlMintActive() public onlyOwner {
 		wlMintActive = !wlMintActive;
 	}
 
-	// Activate public sale
+	// Toggle public sales activity
 	function togglePubMintActive() public onlyOwner {
 		pubMintActive = !pubMintActive;
 	}
